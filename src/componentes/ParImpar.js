72ef26ad05cec3f65e00cc6fe3c64560ce543cea
Moises <moises@rcbdesenvolvimento.com.br>
2018-09-12 23:35:03 -0300

    renderização condicional com function e ternario

--- src/componentes/ParImpar.js
+++ src/componentes/ParImpar.js
@@ -2,12 +2,9 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import Padrao from '../estilo/Padrao'
 
-function parOuImpar(num) {
-    if (num % 2 == 0) {
-        return <Text style={Padrao.ex}>Par</Text>
-    } else {
-        return <Text style={Padrao.ex}>Ímpar</Text>
-    }
+function parOuImpar(num) {    
+    const v = num % 2 == 0 ? 'Par' : 'Ímpar'
+    return <Text style={Padrao.ex}>{v}</Text>
 }
 
 export default props => 
